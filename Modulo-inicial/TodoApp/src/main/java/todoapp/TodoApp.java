cbb36e514b4c5b7c4915ce326da7f31e70a3246d
Wesley <weesleypk@gmail.com>
2023-09-21 12:36:41 -0300

    Organizando arquivos

---
+++ Modulo-inicial/TodoApp/src/main/java/todoapp/TodoApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package todoapp;
+
+/**
+ *
+ * @author Wesley
+ */
+public class TodoApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
